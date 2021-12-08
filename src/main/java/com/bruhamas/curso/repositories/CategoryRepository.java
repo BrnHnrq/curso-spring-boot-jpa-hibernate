cf24050799c475951e01ec048a3fa90f5c50fd21
Bruno Henrique <brunohenriqueoliveiracosta@gmail.com>
2021-12-08 18:32:56 -0300

    Category

---
+++ src/main/java/com/bruhamas/curso/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.bruhamas.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bruhamas.curso.entities.Category;
+
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/bruhamas/curso/services/CategoryServices.java
@@ -0,0 +1,27 @@
+package com.bruhamas.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruhamas.curso.entities.Category;
+import com.bruhamas.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryServices {
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
