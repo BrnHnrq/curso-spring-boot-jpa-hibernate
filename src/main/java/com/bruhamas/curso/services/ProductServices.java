03e317a83e6a733ea1e5210b0e698994d7078954
Bruno Henrique <brunohenriqueoliveiracosta@gmail.com>
2021-12-08 20:16:47 -0300

    Product

---
+++ src/main/java/com/bruhamas/curso/services/ProductServices.java
@@ -0,0 +1,26 @@
+package com.bruhamas.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruhamas.curso.entities.Product;
+import com.bruhamas.curso.repositories.ProductRepository;
+
+@Service
+public class ProductServices {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
